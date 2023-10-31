c51e85f979ad670a5c7482cdd7ae40a566e7ff06
Lowzy <102689882+Lowzy2k@users.noreply.github.com>
2023-10-31 19:38:41 +0200

    Branch attempt
    1 button just for testing

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -133,6 +133,12 @@ public class MainActivity extends AppCompatActivity {
                 othoni1.setText(val);
             }
         });
+        bplus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {othoni1.setText(othoni1.getText() + "+");
+            }
+        });
 
         
         bb1.setOnClickListener(new View.OnClickListener() {
